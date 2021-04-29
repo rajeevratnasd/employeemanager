3240991b3d2bd12e623714cdd2eb1462d6643b15
Rajeev SD <rajeevratnasd@gmail.com>
2021-04-29 14:10:42 +0530

    Cors code added

--- src/main/java/com/rjv/employeemanager/EmployeemanagerApplication.java
+++ src/main/java/com/rjv/employeemanager/EmployeemanagerApplication.java
@@ -2,6 +2,9 @@ package com.rjv.employeemanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeemanagerApplication {
@@ -9,5 +12,14 @@ public class EmployeemanagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/employee/*").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
 }
